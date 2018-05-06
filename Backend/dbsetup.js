a967b3a0248b2688a4a61f30d9bb33a49d2d0193
LJPBOT <lyndon.padfield@gmail.com>
2018-05-06 23:08:34 +1200

    macbook -- push

--- Backend/dbsetup.js
+++ Backend/dbsetup.js
@@ -1,16 +1,44 @@
-// add timestamps in front of log messages
-require('console-stamp')(console, '[HH:MM:ss.l]');
+//Logger -- From Winston
+const { createLogger,format, transports} = require('winston');
+const { combine, timestamp, label, printf, json, colorize } = format;
+require('winston-mongodb');
 
+const myFormat = printf(info => {
+    return `${info.timestamp} [${info.label}] ${info.level}: ${info.message}`;
+  });
+
+const logger = createLogger({
+    transports: [
+      new transports.Console(),
+      new transports.File({ filename: 'combined.log' }),
+      new transports.MongoDB({ db: 'mongodb://192.168.2.12/tvdev' })
+    ],
+   format: combine(
+        timestamp(),
+        label({ label: 'TVDEV - Property'}),
+        myFormat  
+    )
+});
+
+//require('console-stamp')(console, '[HH:MM:ss.l]');
+
+//Mongo - Connection
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://192.168.2.12/tvdev');
 mongoose.connection
-    .once('open', () => console.log('Good to go!'))
+    .once('open', () => logger.info('Database connection successful!'))
     .on('error', (error) => {
-        console.warn('Warning', error);
+        logger.error('Warning', error);
     });
 
+//Connect variables for Address
+//const varaddress = 
+//const varcity =
+//const varstate =
+//const zipcode =
 
+//Add address to property collection
 const Property = require('./src/property');
 
 const addr1 = new Property({ 
@@ -20,8 +48,8 @@ const addr1 = new Property({
     zipcode: '92708'
 });
 addr1.save();
-const timestamp = 
-console.log('Address Written');
+
+logger.info('Address Written');
 
 
 
--- Backend/lib/logger.js
+++ Backend/lib/logger.js
@@ -1,35 +1,32 @@
 const { createLogger,format, transports} = require('winston');
-const { combine, timestamp, label, printf, json } = format;
-
+const { combine, timestamp, label, printf, json, colorize } = format;
+require('winston-mongodb');
 
 const myFormat = printf(info => {
     return `${info.timestamp} [${info.label}] ${info.level}: ${info.message}`;
   });
 
-
 const logger = createLogger({
     transports: [
       new transports.Console(),
-      new transports.File({ filename: 'combined.log' })
+      new transports.File({ filename: 'combined.log' }),
+      new transports.MongoDB({ db: 'mongodb://192.168.2.12/tvdev' })
     ],
-  
    format: combine(
         timestamp(),
         label({ label: 'TVDEV - Property'}),
-        myFormat
-       
+        myFormat  
     )
-  
 });
 
 //
 // Logging
 //
-logger.log({
-    level: 'info',
-    message: 'Hello distributed log files!',
+//logger.log({
+//    level: 'info',
+//    message: 'Hello distributed log files!',
    
 
-  });
+//  });
   
-  logger.error('Hello again distributed logs');
+//  logger.error('Hello again distributed logs');
